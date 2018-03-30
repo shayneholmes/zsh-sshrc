7e35b9890da3745953e63922fe97d2253802f7da
Shayne Holmes <shayneh@qualtrics.com>
2018-03-30 14:12:34 -0700

    Fix sshrc so colors and completion work

--- zsh-sshrc.plugin.zsh
+++ zsh-sshrc.plugin.zsh
@@ -1,12 +1,17 @@
+if [ -z $_color_wrapper ]; then
+  _color_wrapper="_color_from_params"
+fi
+
 _sshrc_wrapper () {
   if [ $# = 1 ]; then
     # no commands; gimme an sshrc shell
-    sshrc "$@"
+    $_color_wrapper sshrc "$@"
   else
     # commands (maybe); just run ssh directly
-    ssh "$@"
+    $_color_wrapper ssh "$@"
   fi
 }
 
 compdef _sshrc_wrapper=ssh
+
 alias ssh="_sshrc_wrapper"
